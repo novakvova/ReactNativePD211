5db6e388294f7b485dd4586da8ce63233d5a2915
novakvova <novakvova@gmail>
2025-04-30 15:59:20 +0300

    Seed Data is good

--- WebCrotApi/WebCrotApi/Data/DbSeeder.cs
+++ WebCrotApi/WebCrotApi/Data/DbSeeder.cs
@@ -4,6 +4,7 @@ using Newtonsoft.Json;
 using System.Text;
 using WebCrotApi.Abstract;
 using WebCrotApi.Constants;
+using WebCrotApi.Data.Entities;
 using WebCrotApi.Data.Entities.Identity;
 using WebCrotApi.Models.Seeder;
 
@@ -54,6 +55,41 @@ public static class DbSeeder
                             await userManager.AddToRoleAsync(newUser, user.Role);
                         }
                         else Console.WriteLine($"--Error create user {user.Email}--");
+                    }
+
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"--Error parse json--{ex.Message}");
+                }
+            }
+            else Console.WriteLine($"--Error open file {jsonFile}--");
+        }
+
+
+        if (!context.Categories.Any())
+        {
+            var imageService = scope.ServiceProvider.GetRequiredService<IImageService>();
+            var jsonFile = Path.Combine(Directory.GetCurrentDirectory(), "Helpers", "JsonData", "Categories.json");
+            if (File.Exists(jsonFile))
+            {
+                var jsonData = File.ReadAllText(jsonFile, Encoding.UTF8);
+                try
+                {
+                    var categories = JsonConvert.DeserializeObject<List<SeederCategoryModel>>(jsonData)
+                        ?? throw new JsonException();
+                    var userManager = scope.ServiceProvider.GetRequiredService<UserManager<UserEntity>>();
+                    foreach (var category in categories)
+                    {
+                        var newCategory = new CategoryEntity
+                        {
+                            Name = category.Name,
+                            Description = category.Description,
+                            UserId=category.UserId,
+                            Image = await imageService.SaveImageFromUrlAsync(category.Image)
+                        };
+                        context.Categories.Add(newCategory);
+                        context.SaveChanges();
                     }
 
                 }
