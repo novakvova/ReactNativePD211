dcc425a48d7a0b5cb3747b9cda35c740af311968
novakvova <novakvova@gmail>
2025-04-30 16:21:47 +0300

    Is good working View Category

---
+++ WebCrotApi/WebCrotApi/Controllers/CategoriesController.cs
@@ -0,0 +1,39 @@
+﻿using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using WebCrotApi.Data;
+using WebCrotApi.Data.Entities.Identity;
+using WebCrotApi.Models.Category;
+
+namespace WebCrotApi.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+[Authorize]
+public class CategoriesController(IMapper mapper,
+    WebCrtopDbContext context, UserManager<UserEntity> userManager) : ControllerBase
+{
+    [HttpGet]
+    public async Task<IActionResult> GetList()
+    {
+        try
+        {
+            string userName = User.Claims.FirstOrDefault().Value;
+            var user = await userManager.FindByEmailAsync(userName);
+            var list = context.Categories
+                .Where(x=>x.UserId==user.Id)
+                .ProjectTo<CategoryItemViewModel>(mapper.ConfigurationProvider)
+                .ToList();
+            return Ok(list);
+        }
+        catch (Exception ex)
+        {
+            return BadRequest(new
+            {
+                invalid = ex.Message
+            });
+        }
+    } 
+}
---
+++ WebCrotApi/WebCrotApi/Mapper/CategoryMapper .cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using WebCrotApi.Data.Entities;
+using WebCrotApi.Models.Category;
+
+namespace WebCrotApi.Mapper;
+
+public class CategoryMapper : Profile
+{
+    public CategoryMapper()
+    {
+        CreateMap<CategoryEntity, CategoryItemViewModel>();
+    }
+}
